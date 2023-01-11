323770fa125af64f9cdc6a68d8b58617cb0c8c4a
Mich01081998 <82514689+Mich01081998@users.noreply.github.com>
2023-01-11 18:16:38 -0400

    Update API_rest.jsx

--- src/servicios/API_rest.jsx
+++ src/servicios/API_rest.jsx
@@ -1,4 +1,4 @@
-export const ApiUrl = "https://primeservidoremi.fly.dev//"
+export const ApiUrl = "https://primeservidoremi.fly.dev/"
 
 // PROHIBIDO BORRAR
 //export const ApiUrl = "https://git.heroku.com/servidor-deploy-emi-prime.git/"

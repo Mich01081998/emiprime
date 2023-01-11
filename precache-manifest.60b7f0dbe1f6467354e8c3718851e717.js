b5d91e4a708e70e7d646520923dc2cbca8516647
Mich01081998 <mcc201998@gmail.com>
2023-01-11 17:53:12 -0400

    Updates

---
+++ precache-manifest.60b7f0dbe1f6467354e8c3718851e717.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7619402d23f62483ec08",
+    "url": "/emiprime/static/css/main.7a1145d1.chunk.css"
+  },
+  {
+    "revision": "7619402d23f62483ec08",
+    "url": "/emiprime/static/js/main.7619402d.chunk.js"
+  },
+  {
+    "revision": "7df15b17d8c53d70c797",
+    "url": "/emiprime/static/css/1.5728b16b.chunk.css"
+  },
+  {
+    "revision": "7df15b17d8c53d70c797",
+    "url": "/emiprime/static/js/1.7df15b17.chunk.js"
+  },
+  {
+    "revision": "edad7614baa9073014ec",
+    "url": "/emiprime/static/js/runtime~main.edad7614.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/emiprime/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "5460feef0c42c74fb748b00e89f6e8ca",
+    "url": "/emiprime/static/media/logo.5460feef.png"
+  },
+  {
+    "revision": "382f417383b95f54819fbb0d7979e929",
+    "url": "/emiprime/static/media/valores.382f4173.png"
+  },
+  {
+    "revision": "4be20492e56b54486f16c37ad5504426",
+    "url": "/emiprime/static/media/quienes_somos.4be20492.png"
+  },
+  {
+    "revision": "ddd6ef77a746b0d559fee1bd8098d623",
+    "url": "/emiprime/static/media/vision.ddd6ef77.png"
+  },
+  {
+    "revision": "e6b6e442a82d187c52ed00987fa1959c",
+    "url": "/emiprime/static/media/mision.e6b6e442.png"
+  },
+  {
+    "revision": "e5e8f06a3770bd85ae39e085c3c4b5fd",
+    "url": "/emiprime/index.html"
+  }
+];
